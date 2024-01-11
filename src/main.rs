6458b81715a3caac7ef5494dc4bea08c91cccf72
JethroNatividad <natividad.jet@gmail.com>
2024-01-11 20:09:45 +0800

    expand problem

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 // Tax calculator, State: winsconsin | illinois (8% tax), if winsconsin - county:  Eau Claire (+0.005) | Dunn (0.004) , other states: no tax.
+// Inputs: order_amount, state, county(only if state: winsconsin)
+// process: calculate tax
+// output: The tax is: {}.\nThe total is {}.
 
 fn main() {
     println!("Hello, world!");
