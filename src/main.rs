a7e1209ff974c2df5ef4fb8775f114b911e9bf03
JethroNatividad <natividad.jet@gmail.com>
2024-01-11 20:07:57 +0800

    write problem

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+// Tax calculator, State: winsconsin | illinois (8% tax), if winsconsin - county:  Eau Claire (+0.005) | Dunn (0.004) , other states: no tax.
+
 fn main() {
     println!("Hello, world!");
 }
