9caf00aea2d45eb2821f3e5689d038e841fa5fcd
JethroNatividad <natividad.jet@gmail.com>
2024-01-11 20:14:10 +0800

    imports

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+use std::io::Write;
 // Tax calculator, State: winsconsin | illinois (8% tax), if winsconsin - county:  Eau Claire (+0.005) | Dunn (0.004) , other states: no tax.
 // Inputs: order_amount, state, county(only if state: winsconsin)
 // process: calculate tax
